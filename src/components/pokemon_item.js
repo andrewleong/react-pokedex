bcf57b0235c28632b1b74336be0a9fc8c1e56a80
darkdrew <andrewleong14@gmail.com>
2017-10-31 18:56:09 +0800

    deleted redundant images

--- src/components/pokemon_item.js
+++ src/components/pokemon_item.js
@@ -10,9 +10,9 @@ class PokemonItem extends Component {
     return(
 
       <div key={id} className="col-sm-4">
-          <Link to={`https://pokeapi.co/api/v1/pokemon/${id}/`}>
+          <Link to={`https://pokeapi.co/api/v2/pokemon/${id}/`}>
               <div className="card each-card">
-                  <img className="card-img-top img-responsive" src={`/sprites/pokemon/model/${id}.png`} alt="" />
+                  <img className="card-img-top img-responsive" src={`/sprites/pokemon/other-sprites/official-artwork/${id}.png`} alt="" />
                       <div className="card-block">
                         <h4 className="card-title card-action">#{id}, {pokemon.name}</h4>
                       </div>
